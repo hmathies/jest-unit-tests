1928bc6d78ed67f271018b898b49bbf5a470d600
Heather Mathies <hlmathies@gmail.com>
2021-09-07 21:32:29 -0400

    formatting code

--- banking/basic-add.test.js
+++ banking/basic-add.test.js
@@ -8,6 +8,7 @@ describe("should return total value of nums", () => {
   test("should return 10", () => {
     expect(addThreeNums(6, 3, 1)).toBe(10);
   });
+
   test("should return 30", () => {
     expect(addABunchOfNums(3, 7, 5, 15, 0)).toBe(30);
   });
